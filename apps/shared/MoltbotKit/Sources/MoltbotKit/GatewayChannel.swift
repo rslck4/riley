7299fea3e9967d70b01bbe137351f66747670a7f
ScottDTX <rslck@me.com>
2026-01-28 19:42:40 -0600

    MoltbotKit: log when gateway requests time out

--- apps/shared/MoltbotKit/Sources/MoltbotKit/GatewayChannel.swift
+++ apps/shared/MoltbotKit/Sources/MoltbotKit/GatewayChannel.swift
@@ -276,7 +276,7 @@ public actor GatewayChannelActor {
             caps: [],
             commands: [],
             permissions: [:],
-            clientId: "moltbot-macos",
+            clientId: "clawdbot-macos",
             clientMode: "ui",
             clientDisplayName: InstanceIdentity.displayName)
         let clientDisplayName = options.clientDisplayName ?? InstanceIdentity.displayName
